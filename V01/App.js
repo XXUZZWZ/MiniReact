a8081efd17e79c6acd152f217c0701cc9ddcfe9d
XXUZZWZ <3189315662@qq.com>
2025-06-11 13:25:06 +0800

    feat(V01):实现了reactdom,render,createElemnet函数
    初步完成了mini-React的main.js ,App,js,以及虚拟dom数据结构的实现和对虚拟dom的渲染，支持渲染各种html类型文件，基本实现了React的api部分，但还没有实现jsx

---
+++ V01/App.js
@@ -0,0 +1,5 @@
+import React from "./core/React.js";
+
+const App = React.createElement("div", { id: "app" }, "Hi- ", "MiniReact");
+
+export default App;
---
+++ V01/main.js
@@ -0,0 +1,87 @@
+//console.log("Hello mini-app!");
+//v1
+// const dom = document.createElement("div");
+// dom.id = "app";
+// document.querySelector("#root").append(dom);
+// const textNode = document.createTextNode("Hello mini-app!");
+// textNode.nodeValue = "app";
+// dom.append(textNode);
+
+// v2 react -> vdom ->js object
+// const textEl = {
+//   type: "TEXT_ELEMENT",
+//   props: {
+//     nodeValue: "app!",
+//     children: [],
+//   },
+// };
+// function createTextNode(text) {
+//   return {
+//     type: "TEXT_ELEMENT",
+//     props: {
+//       nodeValue: text,
+//       children: [],
+//     },
+//   };
+// }
+
+// // const el = {
+// //   type: "div",
+// //   props: {
+// //     id: "app",
+// //     children: [textEl],
+// //   },
+// // };
+
+// function createElement(type, props, ...children) {
+//   return {
+//     type,
+//     props: {
+//       ...props,
+//       children: children.map((child) =>
+//         typeof child === "string" ? createTextNode(child) : child
+//       ),
+//     },
+//   };
+// }
+
+// // const App = createElement("div", { id: "app" }, createTextNode("app"));
+// // const textEl = createTextNode("app");
+// // const dom = document.createElement(App.type);
+// // dom.id = App.props.id;
+// // document.querySelector("#root").append(dom);
+
+// // const textNode = document.createTextNode("");
+// // textNode.nodeValue = textEl.props.nodeValue;
+// // dom.append(textNode);
+// // Uncaught TypeError: Cannot read properties of undefined (reading 'type')
+// function render(el, container) {
+//   const dom =
+//     el.type === "TEXT_ELEMENT"
+//       ? document.createTextNode("")
+//       : document.createElement(el.type);
+//   Object.keys(el.props).forEach((key) => {
+//     if (key !== "children") {
+//       dom[key] = el.props[key];
+//     }
+//   });
+//   const children = el.props.children;
+
+//   children.forEach((child) => {
+//     // 递归调用渲染子节点
+//     render(child, dom);
+//   });
+
+//   container.append(dom);
+// }
+// // const App = createElement("div", { id: "app" }, "hi  ", "mini-react");
+// // const textEl = createTextNode("app");
+// // render(App, document.querySelector("#root"));
+
+// const App = createElement("div", { id: "app" }, "hi  ", "mini-react");
+
+// ReactDOM.createRoot(document.querySelector("#root")).render(App);
+
+import ReactDOM from "./core/ReactDom.js";
+import App from "./App.js";
+ReactDOM.createRoot(document.querySelector("#root")).render(App);
